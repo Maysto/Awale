e179714a94aefb701a98309d722a4fd61af2cb9f
Garnier <corentingarnier06@gmail.com>
2022-03-13 20:49:27 +0100

    refactor

--- src/Game.py
+++ src/Game.py
@@ -7,27 +7,29 @@ class Game:
         self.player1 = isStarting                                              ## True if it is my turn to play esle turn it to false
     
     def _playTurn(self, holePlayed, playedColor):                              ## played is what we receive form the other player ex:(16B)
-        hand = self.board[holePlayed]._empty(playedColor)
+        hand = self.board[holePlayed - 1]._empty(playedColor)
         if playedColor == "B":
-            harvestStart = self._playeBlueSeeds(hand["B"], holePlayed)
+            harvestStart = self._playBlueSeeds(hand["B"], holePlayed)
             self._harvest(harvestStart)
         else:
-            harvestStart = self._playeRedSeeds(hand["R"], holePlayed)
+            harvestStart = self._playRedSeeds(hand["R"], holePlayed)
             self._harvest(harvestStart)
         return self.board
     
-    def _playeRedSeeds(self, seeds, holePlayed):
-        starting = (holePlayed + 1) % 16
+    def _playRedSeeds(self, seeds, holePlayed):
+        if (starting % 16 != 0):
+            starting = staring + 1
         for i in range(seeds):
             self.board[starting]._add_redSeed()
             starting = (starting + 1) % 16
         return starting
     
-    def _playeBlueSeeds(self, seeds, holePlayed):
-        starting = (holePlayed + 1) % 16
+    def _playBlueSeeds(self, seeds, holePlayed):
+        if (starting % 16 != 0):
+            starting = staring + 1
         for i in range(seeds):
             self.board[starting]._add_blueSeed()
-            starting = (starting + 2) % 16
+            starting = (starting + 1) % 16
         return starting
     
     def _harvest(self, start):
@@ -37,7 +39,7 @@ class Game:
         blueSeeds = hole._get_blueSeeds
         redSeeds = hole._get_redSeeds
         total = 0
-        if (blueSeeds + redSeeds == 2 || blueSeeds + redSeeds == 3):
+        if (blueSeeds + redSeeds == 2 or blueSeeds + redSeeds == 3):
             total = hole._empty("R") + hole._empty("B")
             start = start - 1
             return self._harvest(start)
