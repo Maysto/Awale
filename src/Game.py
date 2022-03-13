e2b36043d150e98cfd4d3db920486331cda42d36
Garnier <corentingarnier06@gmail.com>
2022-03-13 15:04:50 +0100

    Harvest func

--- src/Game.py
+++ src/Game.py
@@ -9,18 +9,37 @@ class Game:
     def _playTurn(self, holePlayed, playedColor):                              ## played is what we receive form the other player ex:(16B)
         hand = self.board[holePlayed]._empty(playedColor)
         if playedColor == "B":
-            self._playeBlueSeeds(hand["B"], holePlayed)
+            harvestStart = self._playeBlueSeeds(hand["B"], holePlayed)
+            self._harvest(harvestStart)
         else:
-            self._playeRedSeeds(hand["R"], holePlayed)
+            harvestStart = self._playeRedSeeds(hand["R"], holePlayed)
+            self._harvest(harvestStart)
     
-    def _playeRedSeeds(seeds, holePlayed):
+    def _playeRedSeeds(self, seeds, holePlayed):
         starting = (holePlayed + 1) % 16
         for i in range(seeds):
-            sel.board[starting]._add_redSeed()
+            self.board[starting]._add_redSeed()
             starting = (starting + 1) % 16
+        return starting
     
-    def _playeBlueSeeds(seeds, holePlayed):
+    def _playeBlueSeeds(self, seeds, holePlayed):
         starting = (holePlayed + 1) % 16
         for i in range(seeds):
-            sel.board[starting]._add_blueSeed()
+            self.board[starting]._add_blueSeed()
             starting = (starting + 2) % 16
+        return starting
+    
+    def _harvest(self, start):
+        if (start < 0):
+            start = 15
+        hole = self.board[start]
+        blueSeeds = hole._get_blueSeeds
+        redSeeds = hole._get_redSeeds
+        total = 0
+        if (blueSeeds + redSeeds == 2 || blueSeeds + redSeeds == 3):
+            total = hole._empty("R") + hole._empty("B")
+            start = start - 1
+            return self._harvest(start)
+        else:
+            return total
+
