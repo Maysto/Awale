b9a1ccc641a2104b7c55de5c585b2b09f2808c9b
yohannlaurendeau <wuloan@outlook.fr>
2022-03-14 21:42:12 +0100

    fin jeu condition

--- src/Game.py
+++ src/Game.py
@@ -1,5 +1,6 @@
 from Board import Board
 
+
 class Game:
     """ Classe qui gere les evenments de la partie, fonction pour jouer, etc.... TODO faire la fonction pour jouer un tour de jeu  """
     def __init__(self, isStarting):
@@ -10,6 +11,7 @@ class Game:
     def _playTurn(self, holePlayed, playedColor):                              ## played is what we receive form the other player ex:(16B)
         self.status = "Playing"
         print("Debut du tour : " + str(self.board))
+        
         hand = self.board[holePlayed - 1]._empty(playedColor)
         if playedColor == "B":
             harvestStart = self._playBlueSeeds(hand["B"], holePlayed)
@@ -18,6 +20,11 @@ class Game:
             harvestStart = self._playRedSeeds(hand["R"], holePlayed)
             self._harvest(harvestStart)
         print("Fin du tour : " + str(self.board))
+        limit = 0
+        limit = self._checkend(limit)
+        print("C est la limite"+str(limit))
+        if (limit < 8):
+            self.status = "Finished"
         return self.board
     
     def _playRedSeeds(self, seeds, holePlayed):
@@ -52,7 +59,7 @@ class Game:
         while (blueSeeds + redSeeds == 2 or blueSeeds + redSeeds == 3):
             print(" Je suis dedans")
             
-            total += hole._empty("R") + hole._empty("B")
+            total += hole._empty()
             start = start - 1
             hole = self.board[start]
             blueSeeds = hole._get_blueSeeds()
@@ -60,4 +67,20 @@ class Game:
         else:
             print ("Total du harvest : "+ str(total))
             return total
+
+    def _checkend(self,limit):
+        
+        for i in range(0, 16):
+            print(self.board[i]._get_redSeeds())
+            print(self.board[i]._get_blueSeeds())
+            limit += self.board[i]._get_redSeeds()
+            limit += self.board[i]._get_blueSeeds()
+
+        
+        
+        return limit
+
+
+    
+
 
